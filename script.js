a6caacc62e263ecb90b8ef255aa1cbdce3afb336
Krishanmohan Pachauri <kmpachauri04@gmail.com>
2024-07-08 12:15:01 +0530

    Add files via upload
    Created a Menu or Button Based Chatbot using Javascript.

---
+++ script.js
@@ -0,0 +1,432 @@
+const menuData = {
+  about: {
+    searchText: "aboutC&S_Electric_company_awards_achievements_plants_research_development",
+    text1: "C&S Electric was founded in 1966, with a dream to be a company which plays a vital role in the development of the power infrastructure of India and makes the “Made in India” label respected the world over.",
+    text2: "Please choose from one of the following options.",
+    subMenu: [
+      {
+        name: "Board of Directors",
+        reply1: "Prakash Kumar Chandraker is Managing Director & Chief Executive Officer of C&S Electric Limited. Prakash has over 34 years of versatile leadership experience in the power industry. Prior to his current role, Prakash was Head of the Distribution Systems business in Siemens Limited where he led a diverse, multi-location team.Prior to Siemens, he led the board of Schneider Electric Infrastructure...",
+        subMenu: [
+          { name: "Click to know more..", url: "https://cselectric.co.in/about-us/board-of-directors/"}
+        ] 
+      },
+      {
+        name: "Achievements",
+        url: "https://cselectric.co.in/about-us/Awards-Achievements/",
+      },
+      {
+        name: "Manufacturing Plants",
+        reply1: "C&S have 10 state-of-the-art manufacturing facilities based out of Noida and Delhi. Our plants are spread across 1 million sq ft of area with an employee strength exceeding 5,500+ including 400 engineers.Majority of our plants are ISO:9000 certified and adhere to all statutory EHS compliances. C&S Electric EPC division is ISO: 14001 & OHSAS: 18001 certified for its safety, health & environment initiatives.",
+        subMenu: [
+          { name: "Click to know more..", url: "https://cselectric.co.in/about-us/manufacturing-plants/"}
+        ]
+      },
+      {
+        name: "SnapShot",
+        url: "https://cselectric.co.in/about-us/company-snapshot/",
+      },
+      {
+        name: "Social Responsibility",
+        reply1: "C&S Electric works with various NGO’s for upliftment of underprivileged sections of society Ongoing relationship with Oxfam India to pursue an initiative of right to education act for the children of slums of Kalyanpuri and Trilokpuri.Association with Care India for supporting girl child education program.Blood donation camps, Health check-up, Cloth donation drives are conducted throughout the year.Implementation of environmental friendly practices in all offices and manufacturing locations.",
+        subMenu: [
+          { name: "Click to know more..",  url: "https://cselectric.co.in/about-us/corporate-social-responsibility/"}
+        ]
+      },
+      {
+        name: "Vision",
+        url: "https://cselectric.co.in/about-us/vision-mission-values/",
+      },
+      {
+        name: "Research & Development",
+        url: "https://cselectric.co.in/about-us/research-development/",
+      },
+    ],
+  },
+
+  market: {
+    searchText: "market_segments_power_industries_residental_commercial_agriculture_infrastructure_distribution_transmission_generation_oem",
+    text1: "We at C&S Electric deliver a comprehensive range of electrical and electronic equipment to our esteemed customers which are in sync with the set quality standards.",
+    text2: "Please choose from one of the following options.",
+
+    subMenu: [
+      {
+        name: "Power Generation",
+        url: "https://cselectric.co.in/market-segments/power-generation/",
+      },
+      {
+        name: "Industries",
+        url: "https://cselectric.co.in/market-segments/industries/",
+      },
+      {
+        name: "Residential",
+        url: "https://cselectric.co.in/market-segments/residential-2/",
+      },
+      {
+        name: "Commercial",
+        url: "https://cselectric.co.in/market-segments/commercial/",
+      },
+      {
+        name: "Agriculture",
+        url: "https://cselectric.co.in/market-segments/agriculture/",
+      },
+      {
+        name: "Infrastructure",
+        url: "https://cselectric.co.in/market-segments/infrastructure/",
+      },
+      {
+        name: "Original Equipment Manufacturers (OEM)",
+        url: "https://cselectric.co.in/market-segments/original-equipment-manufacturers-oem/",
+      },
+      {
+        name: "Distribution & Transmission",
+        url: "https://cselectric.co.in/market-segments/distribution-transmission/",
+      },
+    ],
+  },
+
+  news: {
+    searchText: "news_event_media_coverage_corporate",
+    text1: "Please choose from one of the following options.",
+
+    subMenu: [
+      { name: "News", url: "https://cselectric.co.in/news-media/news/" },
+      { name: "Event", url: "https://cselectric.co.in/news-media/event/" },
+      {
+        name: "Media Centers",
+        url: "https://cselectric.co.in/news-media/media-centers/product-videos/",
+      },
+      {
+        name: "Media Coverage",
+        url: "https://cselectric.co.in/news-media/media-coverage/",
+      },
+      {
+        name: "Media Kit",
+        url: "https://cselectric.co.in/news-media/media-kit/",
+      },
+      {
+        name: "Corporate Info",
+        url: "https://cselectric.co.in/news-media/corporate-info/",
+      },
+    ],
+  },
+
+  certifications: {
+    text1: `C&S Electric has all major international product certifications. Its products are tested as per international standards and norms.`,
+
+    subMenu: [
+      {
+        name: "click to know more",
+        url: "https://cselectric.co.in/certifications/",
+      }
+    ]
+  },
+
+  social: {
+    searchText: "social_links_instagram_linkedin_facebook_youtube_twitter_whatsapp",
+    text1: "Follow us on social media",
+
+    subMenu: [
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/company/c&s-electric-ltd-/",
+      },
+      { name: "Twitter", url: "https://x.com/cselectricltd" },
+      {
+        name: "Youtube",
+        url: "https://www.youtube.com/channel/UC4ENJey6g2iCLSddAUCatTA",
+      },
+      { name: "Facebook", url: "https://www.facebook.com/CsElectricLimited/" },
+      {
+        name: "Instagram",
+        url: "https://www.instagram.com/cnselectric_ltd/?hl=en",
+      },
+      {
+        name: "Whatsapp",
+        url: "https://api.whatsapp.com/send/?phone=%2B919599445253&text&type=phone_number&app_absent=0",
+      },
+    ],
+  },
+
+  contact: {
+    searchText: "contact_office_email_Phone_mobile_address_building_customercare_tollfree_number",
+    text1: `Registered Office : 210, 211 & 212, Second Floor, ‘Salcon Aurum’ Building, Plot No. 4, Jasola District Centre, New Delhi- 110025 
+               Email: info@cselectric.co.in 
+               Phone No.: +91-11-6922 5600
+               
+               Customer Care:
+               Email: customercare@cselectric.co.in
+               Toll Free Number :  1800-572-2012`,
+
+    subMenu: [
+      {
+        name: "click to know more",
+        url: "https://cselectric.co.in/contact-us/",
+      },
+    ],
+  },
+
+  products: {
+    searchText: "products_solutions_orders_busducts_switchboards_switchgear_bustrunking_devices_lv",
+    text1: "Please choose from one of the following options.",
+
+    subMenu: [
+      {
+        name: "LV & MV Busducts",
+        url: "https://cselectric.co.in/products-solutions/power-busbars/metal-enclosed-busbar-isobar/",
+      },
+      {
+        name: "LV Switchboards",
+        url: "https://cselectric.co.in/products-solutions/low-voltage-switchboards/",
+      },
+      {
+        name: "LV Switchgear",
+        url: "https://cselectric.co.in/products-solutions/low-voltage-switchgear/",
+      },
+      {
+        name: "LV Bustrunking",
+        url: "https://cselectric.co.in/products-solutions/power-busbars/busbar-trunking/",
+      },
+      {
+        name: "Protection & Measurement Devices",
+        url: "https://cselectric.co.in/products-solutions/protectionandmeasurementdevices/",
+      },
+      {
+        name: "Final distribution Products",
+        url: "https://cselectric.co.in/products-solutions/final-distribution-products/",
+      },
+    ],
+  },
+
+  handleUserInput: function(userInput, isButtonClick = false) {
+    let matched = false;
+      // Iterate through menu items and subMenus
+      for (const menuItem in menuData) {
+        if (menuItem !== "handleUserInput" && this[menuItem].searchText &&
+          this[menuItem].searchText.toLowerCase().includes(userInput.toLowerCase())) {
+            matched = true;
+            // Check for keyword match in text1, text2, and subMenu names
+          if(
+             this[menuItem].text1
+          )
+          {
+            
+            const chatbotText1 = this[menuItem].text1.trim();
+            setTimeout(() => {
+              chatbox.appendChild(createChatLi(chatbotText1, "incoming"));
+              chatbox.scrollTo(0, chatbox.scrollHeight);
+            }, 400);
+  
+            if (this[menuItem].text2) {
+              const chatbotText2 = this[menuItem].text2.trim();
+              setTimeout(() => {
+                chatbox.appendChild(createChatLi(chatbotText2, "incoming"));
+                chatbox.scrollTo(0, chatbox.scrollHeight);
+              }, 800);
+              }
+            // Call handleSubMenuClick for subMenu match (optional)
+            if (this[menuItem].subMenu) {
+              for (const subMenuItem of this[menuItem].subMenu) {
+                const subButton = document.createElement("button");
+                 subButton.textContent = subMenuItem.name;
+                subButton.addEventListener("click", function () {
+               handleSubMenuClick(subMenuItem);
+             });
+             setTimeout(() => {
+              chatbox.appendChild(subButton);
+              chatbox.scrollTo(0, chatbox.scrollHeight);
+             }, 1300);
+             }
+              }
+            }
+          break;
+          }
+      }
+      if(!matched && !isButtonClick){
+        setTimeout(() => {
+          const errorMessage = "Please Choose from available Options to Interact.";
+          chatbox.appendChild(createChatLi(errorMessage, 'incoming'));
+          chatbox.appendChild(createChatLi(` Please reach out to us at customercare@cselectric.co.in & info@cselectric.co.in 
+                                             and Toll Free Number : 1800-572-2012 & Phone No.: +91-11-6922-5600` ,'incoming'));
+        }, 400);
+      }
+    },
+};
+
+
+const chatbox = document.querySelector(".chatbox");
+const chatbotToggler = document.querySelector(".toggler");
+const chatbotRefreshBtn = document.querySelector(".refresh-btn");
+const chatbotCloseBtn = document.querySelector(".close-btn");
+const chatInput = document.querySelector(".input textarea");
+const sendChatBtn = document.querySelector(".input span");
+const menuButtons = document.querySelectorAll(".menubtn");
+
+
+// Function to create chat message list element
+const createChatLi = (message, className) => {
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", className);
+  let chatContent =
+    className === "outgoing"
+      ? `<p>${message}</p>`
+      : `<img src="chatbot.png"><p>${message}</p>`;
+  chatLi.innerHTML = chatContent.trim();
+  return chatLi;
+};
+
+
+//Sending Button Input as an user Input in chatbox
+for (let i = 0; i < menuButtons.length; i++) {
+  menuButtons[i].addEventListener("click", function () {
+    const buttonText = this.textContent.trim().toLowerCase();
+
+    const userMessage = createChatLi(buttonText, "outgoing");
+    chatbox.appendChild(userMessage);
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+  });
+}
+
+
+// Function to handle user input in chatbox
+const handleChat = () => {
+  let userMessage = chatInput.value.trim().toLowerCase();
+  if (!userMessage) {
+    alert("Please Enter a Message.");
+    return;
+  }
+  chatInput.value = "";
+  chatInput.style.height = "auto";
+
+  //Append the user message to the chatbox
+  chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+  
+  menuData.handleUserInput(userMessage);
+};
+
+
+// Handlde Submenu Buttons Click
+function handleSubMenuClick(subMenuItem) {
+  
+  chatbox.appendChild(createChatLi(subMenuItem.name, "outgoing"));
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+
+  if(subMenuItem.reply1){
+    setTimeout(() => {
+      chatbox.appendChild(createChatLi(subMenuItem.reply1,"incoming"));
+      chatbox.scrollTo(0, chatbox.scrollHeight);
+    }, 400);
+  }
+  if(subMenuItem.subMenu){
+    for (const nestedItem of subMenuItem.subMenu){
+      const nestedButton = document.createElement("button");
+      nestedButton.textContent = nestedItem.name;
+      nestedButton.addEventListener('click', function () {
+        handleSubMenuClick(nestedItem);
+        window.open(nestedItem.url, "_blank");
+      });
+      setTimeout(() => {
+        chatbox.appendChild(nestedButton);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+      }, 800);
+    }
+  }
+  else{window.open(subMenuItem.url, "_blank")};
+
+}
+
+
+// Function to update content based on menu item clicked
+function updateContent(menuItem) {
+
+  if (menuItem in menuData && menuData[menuItem].text1) {
+    const chatbotText1 = menuData[menuItem].text1.trim();
+
+    setTimeout(() => {
+      chatbox.appendChild(createChatLi(chatbotText1, "incoming"));
+      chatbox.scrollTo(0, chatbox.scrollHeight);
+    },400);
+  }
+
+  if (menuItem in menuData && menuData[menuItem].text2) {
+    const chatbotText2 = menuData[menuItem].text2.trim();
+
+    setTimeout(() => {
+      chatbox.appendChild(createChatLi(chatbotText2, "incoming"));
+      chatbox.scrollTo(0, chatbox.scrollHeight);
+    },800);
+  }
+
+
+  if (menuItem in menuData && menuData[menuItem].subMenu) {
+    for (const subMenuItem of menuData[menuItem].subMenu) {
+      const subButton = document.createElement("button");
+      subButton.textContent = subMenuItem.name;
+      subButton.addEventListener("click", function () {
+         handleSubMenuClick(subMenuItem);
+      });
+
+      setTimeout(() => {
+        chatbox.appendChild(subButton);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+      },1200);
+    }
+  }
+}
+
+// Add event listeners to menu buttons
+
+menuButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const menuItem= button.id.slice(0, -3); // Extract menu item name from button ID
+    updateContent(menuItem);
+  });
+});
+
+
+// Adjust textarea height dynamically based on input
+chatInput.addEventListener("input", () => {
+  chatInput.style.height = "auto"; //`${inputheight}px`;
+  chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+//Pressing enter will send text to chatbox
+chatInput.addEventListener('keyup', (e) => {
+    if(e.key === "Enter" && !e.shiftkey){
+        e.preventDefault();
+        handleChat();
+    }
+});
+
+// Handle sending chat message
+sendChatBtn.addEventListener("click", handleChat);
+
+// Toggle Chatbot Visibility
+chatbotCloseBtn.addEventListener("click", () =>
+  document.body.classList.remove("show-chatbot")
+);
+chatbotToggler.addEventListener("click", () =>
+  document.body.classList.toggle("show-chatbot")
+);
+
+//Restart Button
+const resetChatbot = () => {
+
+  const incomingMessage = chatbox.querySelectorAll(".incoming");
+  for (let i = 2; i < incomingMessage.length; i++){
+    incomingMessage[i].remove();
+  }
+  const outgoingMessage = chatbox.querySelectorAll(".outgoing");
+  outgoingMessage.forEach(message => message.remove());
+
+  // const MenuBtn = chatbox.querySelectorAll("#menubutton button");
+  const extraButtons = chatbox.querySelectorAll("button");
+  for (let i = 7; i < extraButtons.length; i++) {
+    extraButtons[i].remove();
+  }
+  
+};
+
+chatbotRefreshBtn.addEventListener("click", resetChatbot);
